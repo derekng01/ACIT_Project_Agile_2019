99397727151542612171521d7d26a0272b71c9f5
donkey nong <48866991+derekng01@users.noreply.github.com>
2019-05-13 14:37:45 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-
+const dbiuri = "mongodb+srv://nodeagilefinal:nodeagilefinal@codedatabase-vvcrh.mongodb.net/CodeDatabase"
 var _db = null;
 
 module.exports.getDb = () => {
@@ -7,7 +7,9 @@ module.exports.getDb = () => {
 };
 
 module.exports.init = function(callback) {
-    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    MongoClient.connect(dbiuri, (err, client) => {
+
+        // MongoClient.connect('mongodb://localhost:27017', (err, client) => {
         if (err) {
             return console.log('Unable to connect to DB')
         }
