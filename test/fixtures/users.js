8af5173f3deb0d54fbdb14c65ef4a37502f23487
donkey nong <48866991+derekng01@users.noreply.github.com>
2019-05-17 12:42:28 -0700

    Delete users.js

--- test/fixtures/users.js
+++
@@ -1,5 +0,0 @@
-const { ObjectID: ObjectID } = require('mongodb');
-
-module.exports = [
-{_id:ObjectID(), username:'test', email:'test@test', password:'test',phone:"123",data:"Hi",cssdata:"Bye"}
-]
