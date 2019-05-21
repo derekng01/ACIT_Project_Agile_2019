dfb97d2efa6dc2bb733b9ac36b5225671d9c589a
Zhe Liu <49537953+guagualeee@users.noreply.github.com>
2019-05-21 13:44:11 -0700

    Add files via upload

--- Web Page/js/code.js
+++ Web Page/js/code.js
@@ -1,5 +1,57 @@
-function display_code() {
-    var user_code = document.getElementById('input').value;
-    document.getElementById('output').srcdoc = user_code;
+function chk_auto(){
+    var auto_rend = document.getElementById("autorend").checked;
+
+    if(auto_rend === true){
+        document.getElementById("manrend").disabled = true;
+    }
+    else{
+        document.getElementById("manrend").disabled = false;
+    }
+    return auto_rend;
 }
-window.onload = display_code();
+
+
+function get_input(user_in) {
+    if (user_in === true) {
+        var user_code = document.getElementById('input').value;
+        return user_code
+        // document.getElementById('output').srcdoc = user_code;
+    }
+}
+
+function display_output(user_code,user_in) {
+    if (user_in === true) {
+        var cssstuff = document.getElementById('input2').value;
+        var jsstuff = document.getElementById('input3').value;
+        var code = '<style>' + cssstuff + '</style>'+ user_code + '<script>' + jsstuff + '</script>' ;
+        document.getElementById('output').srcdoc = code
+    }
+}
+
+// function isPrime(number)
+// {
+//     if (number <= 1)
+//         return false;
+//
+//     // The check for the number 2 and 3
+//     if (number <= 3)
+//         return true;
+//
+//     if (number%2 == 0 || number%3 == 0)
+//         return false;
+//
+//     for (var i=5; i*i<=number; i=i+6)
+//     {
+//         if (number%i == 0 || number%(i+2) == 0)
+//             return false;
+//     }
+//
+//     return true;
+// }
+
+module.exports = {
+    display_output,
+    get_input
+}
+
+    // isPrime
